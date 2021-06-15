d49197a437103412beb86dd938f7c470cdbd79b2
Jaime Nufio <jaimeenufio@gmail.com>
2021-06-14 23:14:07 -0400

    heroku port compensation

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-console.log('Listening on 8080');
-app.listen(8080);
+console.log('Listening on 5000');
+app.listen(process.env.PORT || 5000)//.listen(8080);
